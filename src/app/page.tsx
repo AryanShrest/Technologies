b2042e9b170d79cda71d7767857a8a6c6ae22a33
AryanShrest <aryanshrestha0307@gmail.com>
2026-09-10 13:40:10 +0545

    feat: remove CommitmentSection from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import BlogSection from '@/components/home/BlogSection'
 import ClientsSection from '@/components/home/ClientsSection'
-import CommitmentSection from '@/components/home/CommitmentSection'
 import Footer from '@/components/home/Footer'
 import Hero from '@/components/home/Hero'
 import HowWeWork from '@/components/home/HowWeWork'
@@ -27,7 +26,6 @@ export default async function Home() {
         <TeamSection />
         <WhyCoreCraft />
         <StatsFaqSection />
-        <CommitmentSection />
         <BlogSection />
       </main>
       <Footer />
